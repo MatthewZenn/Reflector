11107997b3730a6eb509bd5939037ed445bf6aca
Joshua Zenn <wordman05@gmail.com>
2021-06-18 18:45:21 -0400

    #3 Basic app
    Some code was borrowed from here for a PoC: https://github.com/gorilla/websocket/tree/master/examples/chat

--- reflector-server/go.mod
+++ reflector-server/go.mod
@@ -1,3 +1,9 @@
-module "reflector-server"
+module reflector-server
 
 go 1.13
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/rs/zerolog v1.23.0 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+)
--- reflector-server/main.go
+++ reflector-server/main.go
@@ -2,13 +2,32 @@ package main
 
 import (
 	"github.com/rs/zerolog/log"
-	"time"
+	"net/http"
 )
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Info().Msgf("%s", r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "home.html")
+}
 
 func main() {
 	log.Info().Msg("reflector-server is starting...")
-	for {
-		time.Sleep(1 * time.Second)
-		log.Info().Msg("holding...")
+	hub := newRouter()
+	go hub.run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal().Err(err).Msg("unable to respond to WebSocket requests")
 	}
 }
