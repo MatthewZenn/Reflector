bf29c7cf70406b81210f06e61f9ec989435291a1
Matthew Zenn <spyguy57@gmail.com>
2021-06-20 16:07:14 -0400

    did some stuff with cobra

--- reflector-server/go.mod
+++ reflector-server/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/josephspurrier/goversioninfo v1.2.0 // indirect
 	github.com/rs/zerolog v1.23.0 // indirect
+	github.com/spf13/cobra v1.1.3 // indirect
 	github.com/spf13/viper v1.8.0 // indirect
 	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
 )
--- reflector-server/main.go
+++ reflector-server/main.go
@@ -5,7 +5,37 @@ package main
 import (
 	"github.com/rs/zerolog/log"
 	"net/http"
+	"github.com/spf13/cobra"
+	"fmt"
 )
+
+var (
+	// Used for flags.
+	cfgFile     string
+	userLicense string
+
+	rootCmd = &cobra.Command{
+		Use:   "cobra",
+		Short: "A generator for Cobra based Applications",
+		Long: `Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	}
+)
+
+func init() {
+	
+	rootCmd.AddCommand(versionCmd)
+  }
+  
+  var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of Hugo",
+	Long:  `All software has versions. This is Hugo's`,
+	Run: func(cmd *cobra.Command, args []string) {
+	  fmt.Println("Hugo Static Site Generator v0.9 -- HEAD")
+	},
+  }
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Info().Msgf("%s", r.URL)
